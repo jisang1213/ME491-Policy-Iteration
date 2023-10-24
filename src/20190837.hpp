ea432e6470b5406448f7731e8f1ac601f90b3b7a
Jisang <jisangyou1@gmail.com>
2023-10-25 01:29:38 +0900

    redo

--- src/20190837.hpp
+++ src/20190837.hpp
@@ -2,187 +2,202 @@
 #include <Eigen/Core>
 #include <Eigen/Dense>
 
-int countsquares(Eigen::Vector<int, 12>& state);
-int gameover(Eigen::Vector<int, 12>& state);
-double getOptimalValuehelper(const Eigen::Vector<int, 12>& state, int justscored);
+double value[4096];
+int policy[4095];
+int binarystate;
+int stable;
+int converged;
+
+// void evaluate_policy(int state, int justscored);
+int improve_policy(int state, int justscored);
+void evaluate_policy(int state, int justscored);
 
 /// DO NOT CHANGE THE NAME AND FORMAT OF THIS FUNCTION
 double getOptimalValue(const Eigen::Vector<int, 12>& state){
-  // return the optimal value given the state
-  return getOptimalValuehelper(state, 0);
-}
-
-//Recursive helper function
-double getOptimalValuehelper(const Eigen::Vector<int, 12>& state, int justscored){
-  int greedyaction;
-  Eigen::Vector<int, 12> stateaction;
-  Eigen::Vector<int, 12> nextstate;
-  Eigen::Vector<int, 12> secondturn;
-  int squares_before;
-  int squares_after;
-  int reward=0;
-  int emptyspaces;
-  double optimalvalue=0;
-
-  //If terminal state, return 0.0
-  if(gameover(state)){
-    return 0.0;
-  }
-
-  //greedy action = getOptimalAction(s)
-  greedyaction = getOptimalActionhelper(state,justscored);
-  //find current number of squares
-  squares_before = countsquares(state);
-
-  //do action
-  stateaction = state;
-  stateaction(greedyaction) = 1;
-
-  //check if scored
-  squares_after = countsquares(stateaction);
-
-  if(squares_after>squares_before){
-    reward = squares_after-squares_before;
-    //edge case: game ends on action
-    if(gameover(stateaction)){
-      return reward;
-    }
-    //game continues with another turn if not just scored
-    if(justscored==0){
-      return reward + getOptimalValuehelper(stateaction, 1);
-    }
-  }
-
-  //V* = sum over s' of P(s'|s,a-greedy)[R+getOptimalValue(s')] where R=0
-  //since opponent places randomly, divide sum by number of empty spaces to get average
-
-  //**consider case when opponent places two consecutive lines
-  emptyspaces=0;
-  for(int i=0; i<12; i++){
-    if(stateaction(i)==0){
-      nextstate = stateaction;
-      nextstate(i)=1;
-      if(countsquares(nextstate)>squares_after){
-        for(int j=0; j<12; j++){
-          secondturn = nextstate;
-          secondturn(j)=1;
-          emptyspaces++;
-          optimalvalue += getOptimalValuehelper(secondturn,1);
-        }
-      }
-      else{
-        emptyspaces++;
-        optimalvalue += getOptimalValuehelper(nextstate,0);
-      }
-    }
-  }
-  optimalvalue = reward + optimalvalue/emptyspaces;
-
-  return optimalvalue;  // return optimal value
+  policy_iteration(state);
+  return value[tobinary(state)];
 }
 
 /// DO NOT CHANGE THE NAME AND FORMAT OF THIS FUNCTION
 int getOptimalAction(const Eigen::Vector<int, 12>& state){
-  return getOptimalActionhelper(state, 0);
+  policy_iteration(state);
+  return policy[tobinary(state)];
 }
 
-//Recursive helper function
-int getOptimalActionhelper(const Eigen::Vector<int, 12>& state, int justscored){
-  // return one of the optimal actions given the state.
-  // the action should be represented as a state index, at which a line will be drawn.
-  /// TODO
-  Eigen::Vector<int, 12> stateaction;
-  Eigen::Vector<int, 12> nextstate;
-  Eigen::Vector<int, 12> secondturn;
-  int squares_before;
+void policy_iteration(const Eigen::Vector<int, 12>& state){
+  //store state-value and state-action in array and represent state in binary for index
+  stable=0;
+  converged=0;
+  //represent the state in binary
+  binarystate = tobinary(state);
+
+  //initialize values
+  value[4095] = 0; //4095 is terminal state
+  for(int i=0; i<4095; i++){
+    value[i]=2; //initialize to 2
+  }
+  //initialize policy
+  for(int i=0; i<4095; i++){
+    //search for possible action(empty spaces) from given state and choose first available action
+    for(int j=0; j<12; j++){
+      if(!(i&(1<<j)))
+      policy[i] = j;
+      break;
+    }
+  }
+
+  while(!stable){
+    stable=1;
+    //Evaluate on-policy value recursively starting from given state
+    while(!converged){
+      converged = 1;
+      evaluate_policy(binarystate, 0);
+    }
+    converged=0;
+
+    //policy improvement: set policy as argmax over current value function
+    improve_policy(binarystate, 0);
+  }
+}
+
+void evaluate_policy(int state, int justscored){
+  int reward;
+  int numstates;
+  double valuesum;
+  int squares_prev;
   int squares_after;
-  int reward=0;
-  int emptyspaces;
-  double maxactionvalue=0;
-  double actionvalue;
-  int optimalaction;
+  int stateaction;
+  int nextstate;
 
-  //Loop over a:
-  //argmax over a of sum over s' of P(s'|s,a)[R + getOptimalValue(s')]  (Loop over s')
-  squares_before = countsquares(state);
-  for(int i=0; i<12; i++){
-    if(state(i)==0){
-      actionvalue = 0;
-      //do action
-      stateaction = state;
-      stateaction(i) = 1;
+  if(state == 4095){
+    return; //return if terminal state;
+  }
+  //do action according to policy
+  stateaction = state | policy[state];
+  //check for reward
+  squares_prev = countsquares(state);
+  squares_after = countsquares(stateaction);
+  reward = squares_after-squares_prev;
 
-      //check if scored
-      squares_after = countsquares(stateaction);
+//given an action, the reward is decided.
+// if received reward and not justwon, next state is simply stateaction (evalute this state)
+// if received reward but just won then let opponent play evaluate nextstate and set justwon to 1.
+// if not received reward, then let opponent play, and evaluate nextstate and set justwon to 0.
 
-      if(squares_after>squares_before){
-        reward = squares_after-squares_before;
-        //edge case: game ends on action
-        if(gameover(stateaction)){
-          actionvalue = reward;
-        }
-        else if(justscored==0){
-          actionvalue = reward + getOptimalValuehelper(stateaction, 1);
-        }
+  if(reward && !justscored){
+    nextstate = stateaction;
+    evaluate_policy(nextstate, 1); //another chance
+    if(value[state] != (reward + value[nextstate])){
+      value[state] = reward + value[nextstate]; //update value
+      converged = 0;
+    }
+    return;
+  }
+  else{
+    for(int i=0; i<12; i++){ //update V(s') first
+      if(!(stateaction&(1<<i))){
+        nextstate = stateaction | (1<<i);
+        evaluate_policy(nextstate, 0);
       }
-      else{
-        //V* = sum over s' of P(s'|s,a-greedy)[R+getOptimalValue(s')] where R=0
-        //since opponent places randomly, divide sum by number of empty spaces to get average
-        emptyspaces=0;
-        for(int j=0; j<12; j++){
-          if(stateaction(j)==0){
-            nextstate = stateaction;
-            nextstate(j)=1;
-            if(countsquares(nextstate)>squares_after){
-              for(int k=0; k<12; k++){
-                secondturn = nextstate;
-                secondturn(k)=1;
-                emptyspaces++;
-                actionvalue += getOptimalValuehelper(secondturn,1);
-              }
-            }
-            else{
-              emptyspaces++;
-              actionvalue += getOptimalValuehelper(nextstate,0);
+    }
+  }
+
+  numstates =0;
+  valuesum=0;
+  for(int i=0; i<12; i++){ //loop to sum over V(s')
+    if(!(stateaction&(1<<i))){
+      nextstate = stateaction | (1<<i);
+      valuesum += value[nextstate];
+      numstates++;
+    }
+  }
+
+  if(value[state] != (reward + valuesum/numstates)){
+    value[state] = reward + valuesum/numstates; //update value
+    converged = 0;
+  }
+}
+
+
+//policy improvement
+int improve_policy(int state, int justscored){
+  int reward;
+  int numstates;
+  double valuesum;
+  double actionvalue;
+  double maxactionvalue;
+  int squares_prev;
+  int squares_after;
+  int stateaction;
+  int nextstate;
+  int argmax;
+  int stable;
+
+  //update policy by taking argmax over new values
+  for(int i=0; i<4095; i++){ //for each state
+    maxactionvalue=-1;
+    for(int j=0; j<12; j++){
+      if(!(i&(1<<j))){  //for each possible action
+        //do action
+        stateaction = state | (1<<j);
+        //check for reward
+        squares_prev = countsquares(state);
+        squares_after = countsquares(stateaction);
+        reward = squares_after-squares_prev;
+        if(reward){
+          nextstate = stateaction;
+          actionvalue = reward + value[nextstate];
+        }
+        else{
+          numstates=0;
+          valuesum=0;
+          for(int k=0; k<12; k++){
+            if(!(stateaction&(1<<i))){ //for all s'
+              nextstate = stateaction | (1<<i);
+              valuesum += value[nextstate];
+              numstates++;
             }
           }
+          actionvalue = valuesum/numstates; //since no reward
         }
-        actionvalue = reward + actionvalue/emptyspaces;
-      }
-
-      if(actionvalue>maxactionvalue){
-        maxactionvalue = actionvalue;
-        optimalaction = i;
+        if(actionvalue>maxactionvalue){
+          maxactionvalue = actionvalue;
+          argmax = j;
+        }
       }
     }
+    if(policy[i] != argmax){
+      policy[i] = argmax;
+      stable = 0;
+    }
   }
-
-  return optimalaction;  // return optimal action
 }
 
-int gameover(const Eigen::Vector<int, 12>& state){
-  int end = 1;
+
+//functions
+int tobinary(const Eigen::Vector<int, 12>& state){
+  int binarystate=0;
   for(int i=0; i<12; i++){
-    if(state(i)==0){
-      end = 0;
-    }
+    binarystate |= (state(i) << i);
   }
-  return end;
+  return binarystate;
+  //value = value[binarystate]
+  //action = action[binarystate];
 }
 
-int countsquares(const Eigen::Vector<int, 12>& state){
+int countsquares(int state){
   int count = 0;
-  if(state(0)==1 && state(0)==state(8) && state(8)==state(2) && state(2)==state(6)){
-    count++;
+  if(state&325 == 325){
+    count++; //upper left square
   }
-  if(state(1)==1 && state(1)==state(10) && state(10)==state(3) && state(3)==state(8)){
-    count++;
+  if(state&1290 == 1290){
+    count++; //upper right square
   }
-  if(state(5)==1 && state(5)==state(9) && state(9)==state(3) && state(3)==state(11)){
-    count++;
+  if(state&660 == 660){
+    count++; //bottom left square
   }
-  if(state(4)==1 && state(4)==state(7) && state(7)==state(2) && state(2)==state(9)){
-    count++;
+  if(state&2600 == 2600){
+    count++; //bottom right square
   }
   return count;
 }
